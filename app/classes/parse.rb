a65d34a69528aa5f3b7f2efcd3f818ded9ff0bd2
smlparry <samuel.parry@bigpond.com>
2015-03-01 19:28:50 +1100

    Parses for the address too

--- app/classes/parse.rb
+++ app/classes/parse.rb
@@ -21,7 +21,30 @@ class Parse
         site_content.css('h1 a').each do |name|
             @content[:name] = name.content
         end
+    end
 
+    def address
+        # Need to concatinate street and locality
+        full_address = ''
+
+        # Get street address
+        site_content.css('span.street-address').each do |street_address|
+            full_address << street_address.text.strip
+        end
+
+        # Get locality
+        site_content.css('span.locality').each do |locality|
+            full_address << " " + locality.text.strip
+            full_address = full_address.split(',')[0]
+        end
+
+        # Add full_address to the hash
+        @content[:address] = full_address
+    end
+
+    def do_all
+        self.restaurant_name
+        self.address
         @content
     end
 
