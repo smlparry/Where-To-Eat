dd2080b7ad7e2e4dc9c778d77f54a3f512979989
smlparry <samuel.parry@bigpond.com>
2015-03-03 01:29:07 +1100

    ranking algorithm

--- app/classes/search.rb
+++ app/classes/search.rb
@@ -1,12 +1,27 @@
 class Search
-    def self.rank
+    def self.closest
         location = Location.new
         restaurants = Restaurant.all()
         @proximity = Hash.new
         restaurants.each do |restaurant|
             @proximity[location.distance(restaurant.latitude, restaurant.longitude)] = restaurant.name
         end
-
         @proximity.sort
     end
+
+    def self.rank(price, location = [144.9556878,-37.8053885])
+        location = Location.new
+        # Find within price range
+        items = Item.where("price < ?", price)
+        @rank = Hash.new
+        # Sort by proximity
+        items.each do |item|
+            @rank[location.distance(item.restaurant.latitude, item.restaurant.longitude)] = item.restaurant.name
+        end
+        @rank.sort
+
+        # Algorithm equation
+        # (Rating*No.Items)/(Proximity/MedianPrice)
+    end
+
 end
