ee0382872ab5d13b91283d2d6bd62410e910020f
smlparry <samuel.parry@bigpond.com>
2015-03-03 18:13:48 +1100

    commentz (stalling adding menu items :/)

--- app/classes/patch.rb
+++ app/classes/patch.rb
@@ -1,5 +1,8 @@
 # A class for patching existing records in the database
 class Patch
+    # This finds all the restaurants in the database in which the google api didnt pick
+    # up the longitude and latitude. Mostly due to hitting the api limit but some have
+    # faulty addresses from the urban spoon scraper
     def self.get_failed_log
         restaurants = Restaurant.where(longitude: 1).all()
         location = Location.new
