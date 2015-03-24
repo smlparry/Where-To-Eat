176ef5e329f7fa93d34354ac7142479d2dbd3f66
smlparry <samuel.parry@bigpond.com>
2015-03-24 15:46:10 +1100

    fleshing out basic admin console

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
     before_action :authenticate_admin!
     def restaurants
-        
+        @restaurants = Restaurant.all().order('created_at DESC')
     end
 end
