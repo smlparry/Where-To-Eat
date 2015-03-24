3491a9011405c8fe20fc49e4159a4f479b6881f8
smlparry <samuel.parry@bigpond.com>
2015-03-25 01:00:05 +1100

    Environment variables

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,6 @@
 Rails.configuration.stripe = {
   :publishable_key => "pk_live_35FUw1MUMZZIAyV2gHHK5ntr",
   :secret_key      => "sk_live_IlQoZshQbHTMRd7QsU6yGpLA"
-  ]
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
