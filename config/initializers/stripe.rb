41c4be7ed35e7c21bb947fb9e02cb99c6d98eae5
smlparry <samuel.parry@bigpond.com>
2015-03-25 00:51:48 +1100

    Environment variables

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV["STRIPE_LIVE_PUB"],
-  :secret_key      => ENV["STRIPE_LIVE_SECRET"]
+  :publishable_key => "pk_live_35FUw1MUMZZIAyV2gHHK5ntr",
+  :secret_key      => "sk_live_IlQoZshQbHTMRd7QsU6yGpLA"
   ]
 }
 
