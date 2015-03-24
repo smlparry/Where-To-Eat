6d78eebb8bfbd05def3ddd16d5e539b7d4fcd944
smlparry <samuel.parry@bigpond.com>
2015-03-24 23:49:57 +1100

    added stripe env variables, took out turbo links

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => "pk_test_y8ch8eh6c2Jvgv1sjrn8ELFT",
-  :secret_key      => "sk_test_PDUOaztkWIpJkxpFLrrOhv2c"
+  :publishable_key => ENV['STRIPE_KEY_PUB'],
+  :secret_key      => ENV['STRIPE_KEY_SECRET']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
